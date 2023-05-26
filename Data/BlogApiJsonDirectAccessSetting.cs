07fd28d07c44d40cc6d0d816a32c0d6855b58370
Leonardo Pérez <leo.prez.k@gmail.com>
2023-05-26 16:37:44 +0000

    first commit

---
+++ Data/BlogApiJsonDirectAccessSetting.cs
@@ -0,0 +1,9 @@
+namespace Data;
+
+public class BlogApiJsonDirectAccessSetting
+{
+    public string BlogPostFolder { get; set; } = string.Empty;
+    public string CategoriesFolder { get; set; } = string.Empty;
+    public string TagsFolder { get; set; } = string.Empty;
+    public string DataPath { get; set; } = string.Empty;
+}
